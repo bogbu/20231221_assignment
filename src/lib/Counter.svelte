b225e2a55365c7bd26f4adc0af378b4d93266bf0
bogbu <glutamine082442@gmail.com>
2023-12-21 15:41:46 +0900

    카운터 함수 수정

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   let count: number = 0
   const increment = () => {
-    count += 1
+    count += 2
   }
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+
