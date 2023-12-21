1979ae7ea09218b9bfd07ead6f609767e36697e6
bogbu <glutamine082442@gmail.com>
2023-12-21 17:55:34 +0900

    createTodo 추가

--- api/abc.ts
+++ api/abc.ts
@@ -5,17 +5,17 @@ export default function (req: any, res: any) {
     })
 }
 
-async function createTodo() {
+export async function createTodo() {
     const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
-        method: 'POST',
+        method: 'GET',
         headers: {
             'content-type': 'application/json',
             'apikey': 'KDT7_GrZ1eYBo', // KDT 7기 APIKEY 입니다!
             'username': 'KDT7_ParkYoungWoong'
         },
-        body: JSON.stringify({
-            title: '아침 먹기!'
-        })
+        // body: JSON.stringify({
+        //     title: '아침 먹기!'
+        // })
     })
     const json = await res.json()
     console.log(json)
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import {createTodo} from "../../api/abc";
   let count: number = 0
   const increment = () => {
     count += 2
   }
+  // createTodo();
 </script>
 
 <button on:click={increment}>
