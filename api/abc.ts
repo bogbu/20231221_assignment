fbc9f6be6ff6e17d4024161ba09e479112e369e2
bogbu <glutamine082442@gmail.com>
2023-12-21 16:43:23 +0900

    createTodo 추가

--- api/abc.ts
+++ api/abc.ts
@@ -4,3 +4,21 @@ export default function (req: any, res: any) {
         name: 'jung-ji-hye'
     })
 }
+
+async function createTodo() {
+    const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'apikey': 'KDT7_GrZ1eYBo', // KDT 7기 APIKEY 입니다!
+            'username': 'KDT7_ParkYoungWoong'
+        },
+        body: JSON.stringify({
+            title: '아침 먹기!'
+        })
+    })
+    const json = await res.json()
+    console.log(json)
+
+    return json
+}
